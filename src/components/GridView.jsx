973dde2f039154ecd7f5f4132d4efff5d9bf580a
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-09-22 12:33:32 -0300

    fix: fix display of the modal and add a close button

--- src/components/GridView.jsx
+++ src/components/GridView.jsx
@@ -1,15 +1,40 @@
+import { useState } from 'react';
 import styles from '../styles/GridView.module.css'
 import Card from './Card';
+import Modal from './Modal';
 
 function GridView({ data }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedCountry, setSelectedCountry] = useState({})
+
+  function showModal(countryInfo) {
+    setIsOpen(true)
+    setSelectedCountry(countryInfo)
+  }
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
   return (
-    <div className={styles.grid}>
-      {data?.map((country, index) => {
-        return(
-          <Card key={index} country={country} />
-        )
-      })}
-    </div>
+    <>
+      <div className={styles.grid}>
+        {data?.map((country, index) => {
+          return(
+            <Card 
+              key={index} 
+              country={country} 
+              handleClick={() => showModal(country)} 
+            />
+          )
+        })}
+      </div>
+      <Modal
+        isOpen={isOpen} 
+        handleClose={closeModal}
+        country={selectedCountry} 
+      />
+    </>
   );
 }
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react';
 import styles from '../styles/Modal.module.css'
 
-function Modal({ country }) {
+function Modal({ country, isOpen, handleClose }) {
   const {
     name,
-    currencies,
-    capital,
-    languages,
-    latlng,
-    flag,
-    maps,
-    population,
-    timezones,
-    continents,
-    flags,
-    coatOfArms,
+    // currencies,
+    // capital,
+    // languages,
+    // latlng,
+    // flag,
+    // maps,
+    // population,
+    // timezones,
+    // continents,
+    // flags,
+    // coatOfArms,
   } = country
 
   return (
-    <div className={styles.overlay}>
+    <div className={`${styles.overlay} ${isOpen && styles.show}`}>
       <div className={styles.modal}>
-        <h2>{name.common}</h2>
+        <button onClick={handleClose}>X</button>
+        <h2>{name?.common}</h2>
       </div>
     </div>
   );
